0a8f63a1006bb8b52c416cb6425194572d5e281c
Quinnan Gill <quinnan.gill@gmail.com>
2019-08-04 17:14:06 -0600

    Added captain profile

--- src/body/team/assets/team.js
+++ src/body/team/assets/team.js
@@ -3,24 +3,14 @@ const img_path = process.env.PUBLIC_URL + "/img/officers/"
 const team = {
     "team": [
         {
-            "name": "George Washington",
-            "position": "President",
-            "image": img_path + "officer_helmet.jpg",
-            "descript": [
-                "I am the Pitt Rocketry Club's President. ",
-                "I have been fascated by rockets since I was young.",
-                "In my spare time",
-                "I like to chop down cherry trees. ",
-                "I am the Pitt Rocketry Club's President. ",
-                "I have been fascated by rockets since I was young.",
-                "In my spare time",
-                "I like to chop down cherry trees. ",
-                "I am the Pitt Rocketry Club's President. ",
-                "I have been fascated by rockets since I was young.",
-                "In my spare time",
-                "I like to chop down cherry trees.",
-            ],
-            skills: ["rockets", "electronics", "programming", "mechanical"]
+        "name": "Eric Sherbocker",
+        "position": "Captain",
+        "image": img_path + "",
+        "descript": [
+            "I am a junior Mechanical Engineering student and am currently serving as our team captain and manufacturing lead.",
+            "I am looking forward to a successful year with our team! Please email me at, ems261@pitt.edu, with any questions regarding our organization."
+        ],
+        skills: ["Mechanical Design", "Manufacturing", "SolidWorks Modeling"]
         },
         {
             "name": "Thomas Jefferson",
