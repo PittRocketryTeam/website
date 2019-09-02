d14fb5c6a3d8d6a356773fb34acfb3acd70f62e6
danielstumpp <dcs98@pitt.edu>
2019-09-02 16:16:15 -0400

    updated meeting time

--- src/body/home/Home.js
+++ src/body/home/Home.js
@@ -1,38 +1,36 @@
 import React from "react";
-import {Image} from "react-bootstrap";
+import { Image } from "react-bootstrap";
 import Sponsors from "./sponsors/Sponsors";
 import "./assets/style_home.css";
 
 function Home() {
-    return (
+  return (
+    <div>
+      <div className="home">
+        <Image src={process.env.PUBLIC_URL + "/img/space.png"} fluid />
+        <h1 className="mission-statement">Mission Statement</h1>
+        <h3 className="statement">
+          To grow a community in Pittsburgh dedicated to advancing human
+          spaceflight technology
+        </h3>
+      </div>
+      <div className="w-and-w">
         <div>
-            <div className="home">
-                <Image src={process.env.PUBLIC_URL + "/img/space.png"} fluid />
-                <h1 className="mission-statement">
-                    Mission Statement
-                </h1>
-                <h3 className="statement">
-                    To grow a community in Pittsburgh dedicated to advancing
-                    human spaceflight technology
-                </h3>
-            </div>
-            <div className="w-and-w">
-                <div>
-                    <div className="w">
-                        <h3>Where</h3>
-                    </div>
-                    <strong>Benedum 303</strong>
-                </div>
-                <div>
-                    <div className="w">
-                        <h3>When</h3>
-                    </div>
-                    <strong>Thursdays 6:00</strong>
-                </div>
-            </div>
-            <Sponsors />
+          <div className="w">
+            <h3>Where</h3>
+          </div>
+          <strong>Benedum 319</strong>
         </div>
-    )
+        <div>
+          <div className="w">
+            <h3>When</h3>
+          </div>
+          <strong>Wednesdays 8:00pm</strong>
+        </div>
+      </div>
+      <Sponsors />
+    </div>
+  );
 }
 
 export default Home;
